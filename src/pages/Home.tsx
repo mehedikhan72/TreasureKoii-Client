54bdc2a16981bbc5fe771d85d79e55bf97d61e38
mehedikhan72 <lopingcard@gmail.com>
2023-11-27 15:38:35 +0600

    auth requests are made using axios

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from "react";
+import React, { useContext } from "react";
 import AuthContext from "../utils/context/AuthContext";
 
 const Home: React.FC = () => {
--- src/utils/context/AuthContext.tsx
+++ src/utils/context/AuthContext.tsx
@@ -1,11 +1,6 @@
 // src/AuthContext.tsx
-import React, {
-  createContext,
-  useState,
-  useEffect,
-  FC,
-} from "react";
-import jwt_decode, {JwtPayload} from 'jwt-decode';
+import React, { createContext, useState, useEffect, FC } from "react";
+import { JwtPayload } from "jwt-decode";
 import { jwtDecode } from "jwt-decode";
 import { useNavigate } from "react-router-dom";
 import { AuthContextProps, AuthProviderProps, AuthTokens } from "../../types";
@@ -16,46 +11,45 @@ const AuthContext = createContext<AuthContextProps | undefined>(undefined);
 export default AuthContext;
 
 export const AuthProvider: FC<AuthProviderProps> = ({ children }) => {
-    const [authTokens, setAuthTokens] = useState<AuthTokens | null>(null);
-    const [user, setUser] = useState<JwtPayload | null>(null);
+  const [authTokens, setAuthTokens] = useState<AuthTokens | null>(null);
+  const [user, setUser] = useState<JwtPayload | null>(null);
 
-    useEffect(() => {
-      const storedTokens = localStorage.getItem('authTokens');
-    
-      if (storedTokens) {
-        setAuthTokens(JSON.parse(storedTokens));
-        setUser(jwtDecode(storedTokens));
-      }
-    }, []);
+  useEffect(() => {
+    const storedTokens = localStorage.getItem("authTokens");
+
+    if (storedTokens) {
+      setAuthTokens(JSON.parse(storedTokens));
+      setUser(jwtDecode(storedTokens));
+    }
+  }, []);
   const [loading, setLoading] = useState(true);
   const [message, setMessage] = useState<string | null>(null);
 
   const navigate = useNavigate();
 
-  // todo: need to make these fetch requests using axios
-
   const loginUser = async (
     e: React.FormEvent<HTMLFormElement>
   ): Promise<void> => {
     e.preventDefault();
-    let response = await fetch(`http://127.0.0.1:8000/api/token/`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        email: e.currentTarget.email.value,
-        password: e.currentTarget.password.value,
-      }),
-    });
-    let data = await response.json();
-    if (response.status === 200) {
-      setAuthTokens(data);
-      setUser(jwtDecode(data.access));
-      localStorage.setItem("authTokens", JSON.stringify(data));
-      navigate("/");
-    } else {
-      setMessage(data.detail);
+    const formData = {
+      email: e.currentTarget.email.value,
+      password: e.currentTarget.password.value,
+    };
+
+    try {
+      let response = await axios.post(`token/`, formData);
+      console.log(response);
+      let data = response.data;
+      if (response.status === 200) {
+        setAuthTokens(data);
+        setUser(jwtDecode(data.access));
+        localStorage.setItem("authTokens", JSON.stringify(data));
+        navigate("/");
+      } else {
+        setMessage(data.detail);
+      }
+    } catch (error) {
+      console.log(error);
     }
   };
 
@@ -69,20 +63,25 @@ export const AuthProvider: FC<AuthProviderProps> = ({ children }) => {
   };
 
   const updateToken = async (): Promise<void> => {
-    let response = await fetch(`http://127.0.0.1:8000/api/token/refresh/`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ refresh: authTokens?.refresh }),
-    });
-    let data = await response.json();
+    const formData = {
+      refresh: authTokens?.refresh,
+    };
 
-    if (response.status === 200) {
-      setAuthTokens(data);
-      setUser(jwtDecode(data.access));
-      localStorage.setItem("authTokens", JSON.stringify(data));
-    } else {
+    try {
+      let response = await axios.post(`token/refresh/`, formData);
+      console.log(response);
+      let data = response.data;
+      console.log(data);
+
+      if (response.status === 200) {
+        setAuthTokens(data);
+        setUser(jwtDecode(data.access));
+        localStorage.setItem("authTokens", JSON.stringify(data));
+      } else {
+        logoutUser();
+      }
+    } catch (error) {
+      console.log(error);
       logoutUser();
     }
 
@@ -96,10 +95,11 @@ export const AuthProvider: FC<AuthProviderProps> = ({ children }) => {
       updateToken();
     }
 
-    let fourMins = 1000 * 4 * 60;
+    let fourMins = 1000 * 1 * 30;
     let interval = setInterval(() => {
       if (authTokens) {
         updateToken();
+        console.log("Token refreshed");
       }
     }, fourMins);
     return () => clearInterval(interval);
