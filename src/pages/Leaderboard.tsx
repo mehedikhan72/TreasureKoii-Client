d6bca30120c9686e8a16e3152010dfaf431f4109
mehedikhan72 <lopingcard@gmail.com>
2023-12-06 01:03:40 +0600

    leaderboard done

--- src/pages/Leaderboard.tsx
+++ src/pages/Leaderboard.tsx
@@ -36,16 +36,36 @@ const Leaderboard: React.FC = () => {
     getHuntDetails();
   }, [slug]);
   return (
-    <div>
+    <div className="overflow-x-hidden">
       <HuntNav slug={slug} huntName={hunt?.name} />
-      <p>Leaderboard</p>
-      {leaderBoard.map((team: any) => (
-        <div key={team.id}>
-          <p>
-            {team.team_name} - {team.points}
-          </p>
+      <p className="text-3">Leaderboard</p>
+      <div className="w-full">
+        <div className="flex flex-col">
+          <div className="flex justify-between p-2">
+            <p className="flex-1 text-2">Team Name</p>
+            <p className="flex-1 text-2">Points</p>
+          </div>
+          {leaderBoard.map((team: any, index: number) => (
+            <div
+              key={team.id}
+              className={`flex justify-between p-2 ${
+                index === 0
+                  ? "bg-green-600 hover:scale-105"
+                  : index === 1
+                  ? "bg-green-400 hover:scale-105"
+                  : index === 2
+                  ? "bg-yellow-300 hover:scale-105"
+                  : index % 2 === 0
+                  ? "bg-gray-100 hover:scale-105"
+                  : "bg-gray-200 hover:scale-105"
+              }`}
+            >
+              <p className="flex-1 text-1">{team.team_name}</p>
+              <p className="flex-1 text-1">{team.points}</p>
+            </div>
+          ))}
         </div>
-      ))}
+      </div>
     </div>
   );
 };
