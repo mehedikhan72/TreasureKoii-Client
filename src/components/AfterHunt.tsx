9a555c7f2ff603fbb9e763ac519e6aa8a386fc78
FahadPC <mumufahad@gmail.com>
2023-12-06 20:23:57 +0600

    Fahad : AfterHunt

--- src/components/AfterHunt.tsx
+++ src/components/AfterHunt.tsx
@@ -1,9 +1,77 @@
-import React from 'react'
+import { AxiosError } from "axios";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { Hunt } from "../types";
+import axios from "../utils/axios/AxiosSetup";
+import HomeFooter from "./HomeFooter";
+import LeaderboardTable from "./LeaderboardTable";
+import ShowImages from "./ShowImages";
 
 const AfterHunt: React.FC = () => {
-  return (
-    <div>AfterHunt</div>
-  )
-}
+	const { slug } = useParams();
 
-export default AfterHunt
+	const [leaderBoard, setLeaderBoard] = useState<[]>([]);
+	const [hunt, setHunt] = useState<Hunt>();
+
+	const [message, setMessage] = useState<string | null>();
+
+	const fetchHuntData = async (): Promise<void> => {
+		try {
+			const response = await axios.get(`/hunt/${slug}`);
+			const data = response.data;
+
+			if (response.status === 200) {
+				setHunt(data as Hunt);
+			} else {
+				setMessage(data.error);
+			}
+		} catch (error) {
+			if (error instanceof AxiosError) {
+				setMessage(error.response?.data.error);
+			}
+		}
+	};
+
+	const getLeaderBoard = async (): Promise<void> => {
+		try {
+			const response = await axios.get(`${slug}/leaderboard/`);
+			const data = response.data;
+			if (response.status === 200) {
+				setLeaderBoard(data);
+			}
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
+	useEffect(() => {
+		Promise.all([fetchHuntData(), getLeaderBoard()]);
+	}, []);
+
+	return (
+		<div className="flex flex-col min-h-screen">
+			{message && <p>{message}</p>}
+
+			{hunt && (
+				<>
+					<div className="flex flex-col justify-center items-center mt-16 mx-10">
+						<div className="text-3 text-center">Here is a summary for this hunt</div>
+						<div className="text-4xl">{hunt.name}</div>
+						<div className="text-lg my-2">{`${new Date(hunt.start_date).toDateString()} - ${new Date(
+							hunt.end_date
+						).toDateString()}`}</div>
+						<img src={hunt.poster_img} alt="Hunt Image" className="w-4/5 max-h-72 object-contain my-5" />
+						<div className="text-xl">{hunt.description}</div>
+					</div>
+					<div className="text-3 text-center mt-16">Look back at the memories : </div>
+					<ShowImages url={`${slug}/get-hunt-images/`} />
+					<LeaderboardTable leaderBoard={leaderBoard} />
+				</>
+			)}
+
+			<HomeFooter />
+		</div>
+	);
+};
+
+export default AfterHunt;
--- src/pages/Leaderboard.tsx
+++ src/pages/Leaderboard.tsx
@@ -3,71 +3,45 @@ import { useParams } from "react-router-dom";
 import axios from "../utils/axios/AxiosSetup";
 import { Hunt } from "../types";
 import HuntNav from "../components/HuntNav";
+import LeaderboardTable from "../components/LeaderboardTable";
 
 const Leaderboard: React.FC = () => {
-  const { slug } = useParams();
+	const { slug } = useParams();
 
-  const [leaderBoard, setLeaderBoard] = useState<[]>([]);
-  const [hunt, setHunt] = useState<Hunt>();
-  useEffect(() => {
-    const getLeaderBoard = async (): Promise<void> => {
-      try {
-        const response = await axios.get(`${slug}/leaderboard/`);
-        const data = response.data;
-        if (response.status === 200) {
-          setLeaderBoard(data);
-        }
-      } catch (error) {
-        console.log(error);
-      }
-    };
-    const getHuntDetails = async (): Promise<void> => {
-      try {
-        const response = await axios.get(`hunt/${slug}/`);
-        const data = response.data;
-        if (response.status === 200) {
-          setHunt(data);
-        }
-      } catch (error) {
-        console.log(error);
-      }
-    };
-    getLeaderBoard();
-    getHuntDetails();
-  }, [slug]);
-  return (
-    <div className="overflow-x-hidden">
-      <HuntNav slug={slug} huntName={hunt?.name} />
-      <p className="text-3">Leaderboard</p>
-      <div className="w-full px-4">
-        <div className="flex flex-col">
-          <div className="flex justify-between p-2">
-            <p className="flex-1 text-2">Team Name</p>
-            <p className="flex-1 text-2">Points</p>
-          </div>
-          {leaderBoard.map((team: any, index: number) => (
-            <div
-              key={team.id}
-              className={`flex justify-between p-2 ${
-                index === 0
-                  ? "bg-green-600 hover:scale-105"
-                  : index === 1
-                  ? "bg-green-400 hover:scale-105"
-                  : index === 2
-                  ? "bg-yellow-300 hover:scale-105"
-                  : index % 2 === 0
-                  ? "bg-gray-100 hover:scale-105"
-                  : "bg-gray-200 hover:scale-105"
-              }`}
-            >
-              <p className="flex-1 text-1">{team.team_name}</p>
-              <p className="flex-1 text-1">{team.points}</p>
-            </div>
-          ))}
-        </div>
-      </div>
-    </div>
-  );
+	const [leaderBoard, setLeaderBoard] = useState<[]>([]);
+	const [hunt, setHunt] = useState<Hunt>();
+	useEffect(() => {
+		const getLeaderBoard = async (): Promise<void> => {
+			try {
+				const response = await axios.get(`${slug}/leaderboard/`);
+				const data = response.data;
+				if (response.status === 200) {
+					setLeaderBoard(data);
+				}
+			} catch (error) {
+				console.log(error);
+			}
+		};
+		const getHuntDetails = async (): Promise<void> => {
+			try {
+				const response = await axios.get(`hunt/${slug}/`);
+				const data = response.data;
+				if (response.status === 200) {
+					setHunt(data);
+				}
+			} catch (error) {
+				console.log(error);
+			}
+		};
+		getLeaderBoard();
+		getHuntDetails();
+	}, [slug]);
+	return (
+		<div className="overflow-x-hidden">
+			<HuntNav slug={slug} huntName={hunt?.name} />
+			<LeaderboardTable leaderBoard={leaderBoard} />
+		</div>
+	);
 };
 
 export default Leaderboard;
