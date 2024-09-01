e22ab3295744cb65bed8166343ca4da43f82e5a7
Mustafa Muhaimin <mumufahad@gmail.com>
2024-09-01 21:59:39 +0600

    Home page image fix

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -25,27 +25,18 @@ const Home: React.FC = () => {
     <>
       <div>
         <TreasureKoiiImg />
-        <p className="text-3 mx-2">
-          TreasureKoii: Your ultimate solution for all your treasure hunt
-          adventures.
-        </p>
+        <p className="text-3 mx-2">TreasureKoii: Your ultimate solution for all your treasure hunt adventures.</p>
       </div>
 
       <div className="flex justify-center items-center mt-5">
-        <img
-          src="https://escapetrails.co/wp-content/uploads/2020/03/Treasure-hunt.jpg"
-          className="w-5/6 md:w-1/2 rounded-xl"
-          alt="treasure hunt"
-        />
+        <img src="../treasure-koii-logo.png" className="w-5/6 md:w-1/2 rounded-xl" alt="treasure hunt" />
       </div>
 
       <div className="flex justify-center items-center mt-2">
         <p className="text-2 mx-5 max-w-3xl">
-          Empower your treasure hunt adventures with our platform – your
-          all-in-one solution for creating captivating, interactive, and
-          personalized treasure hunts. Unleash creativity, engage participants,
-          and turn any location into an exciting journey of discovery. Start
-          crafting unforgettable experiences today!
+          Empower your treasure hunt adventures with our platform – your all-in-one solution for creating captivating,
+          interactive, and personalized treasure hunts. Unleash creativity, engage participants, and turn any location
+          into an exciting journey of discovery. Start crafting unforgettable experiences today!
         </p>
       </div>
 
