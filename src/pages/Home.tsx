d4f5fb9f8c62831c3bf2e72acad39d898870e52b
Mustafa Muhaimin <mumufahad@gmail.com>
2024-09-07 00:45:14 +0600

    leaderboard, homepage hunt list

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -50,7 +50,6 @@ const Home: React.FC = () => {
 				</Link>
 			</div>
 			<OrganizingHunts />
-			<RegisteredHunts />
 			<RecentlyHosted />
 			<HomeFooter />
 		</>
--- src/utils/axios/AxiosSetup.tsx
+++ src/utils/axios/AxiosSetup.tsx
@@ -1,46 +1,42 @@
 import axios from "axios";
 
-import { JwtPayload } from "jwt-decode";
-import { jwtDecode } from "jwt-decode";
-
-import { useState, useContext } from "react";
-import AuthContext from "../context/AuthContext";
+import { jwtDecode, JwtPayload } from "jwt-decode";
 
 // const instance = axios.create({
 //     baseURL: 'https://api.treasurekoii.com/api/'
 // });
 
 const instance = axios.create({
-  baseURL: "http://localhost:8000/api/",
+	baseURL: "http://localhost:8000/api/",
 });
 
 // Add a request interceptor to add the access token to the request headers
 instance.interceptors.request.use((config) => {
-  const authTokens = localStorage.getItem("authTokens");
+	const authTokens = localStorage.getItem("authTokens");
 
-  if (authTokens) {
-    const accessToken = JSON.parse(authTokens).access;
-    // if (isTokenExpired(accessToken)) {
-    //   const contextData = useContext(AuthContext);
-    //   const updateToken = contextData?.updateToken;
-    //   if (updateToken) {
-    //     console.log("calling update token from axios setup");
-    //     updateToken();
-    //   }
-    // }
+	if (authTokens) {
+		const accessToken = JSON.parse(authTokens).access;
+		// if (isTokenExpired(accessToken)) {
+		//   const contextData = useContext(AuthContext);
+		//   const updateToken = contextData?.updateToken;
+		//   if (updateToken) {
+		//     console.log("calling update token from axios setup");
+		//     updateToken();
+		//   }
+		// }
 
-    config.headers.Authorization = `Bearer ${accessToken}`;
-  }
-  return config;
+		config.headers.Authorization = `Bearer ${accessToken}`;
+	}
+	return config;
 });
 
 function isTokenExpired(token: string) {
-  const decodedToken: JwtPayload = jwtDecode(token);
-  const currentTime = Date.now() / 1000; // Convert milliseconds to seconds
-  if (!decodedToken.exp) {
-    return false;
-  }
-  return decodedToken.exp < currentTime;
+	const decodedToken: JwtPayload = jwtDecode(token);
+	const currentTime = Date.now() / 1000; // Convert milliseconds to seconds
+	if (!decodedToken.exp) {
+		return false;
+	}
+	return decodedToken.exp < currentTime;
 }
 
 export default instance;
