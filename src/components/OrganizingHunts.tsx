f674edc318398a4650df7c9709d40a28e51b9c4c
Mustafa Muhaimin <mumufahad@gmail.com>
2024-09-07 01:48:18 +0600

    blah

--- src/components/OrganizingHunts.tsx
+++ src/components/OrganizingHunts.tsx
@@ -5,13 +5,29 @@ import { Hunt } from "../types";
 import { Link } from "react-router-dom";
 import AuthContext from "../utils/context/AuthContext";
 
-const countdownRenderer = ({ days, hours, minutes, seconds, completed }: any) => {
+const zeroPad = (num: number, places: number) => String(num).padStart(places, "0");
+
+const countdownRenderer = ({
+	days,
+	hours,
+	minutes,
+	seconds,
+	completed,
+}: {
+	days: number;
+	hours: number;
+	minutes: number;
+	seconds: number;
+	[key: string]: any;
+}) => {
 	if (completed) {
 		return <span>The Hunt Has Commenced</span>;
 	} else {
+		const date = new Date(days, hours, minutes, seconds);
 		return (
 			<span>
-				{!!days && `${days}D`} {!!hours && `${hours}H`} {!!minutes && `${minutes}M`} {!!seconds && `${seconds}S`}
+				{!!days && `${days}D`} {!!days && `${zeroPad(hours, 2)}H`} {!!days && !!hours && `${zeroPad(minutes, 2)}M`}{" "}
+				{!!days && !!hours && !!minutes && `${zeroPad(seconds, 2)}S`}
 			</span>
 		);
 	}
@@ -39,43 +55,36 @@ const OrganizingHunts: React.FC = () => {
 		if (user) getRegisteredHunts();
 	}, [contextData]);
 
-	return (
-		<div>
-			{hunts?.length ? (
-				<div className="flex flex-col justify-center items-center my-5 mx-8">
-					<div className="text-4 my-5 leading-[3rem]">
-						You're <span className="bg-slate-700 rounded-lg text-white px-4 py-2">organizing</span> the following
-						hunt(s)
-					</div>
-					<div className="m-2 sm:m-4 rounded-md grid grid-cols-[1fr_auto_auto] styled-div-1 p-0 [&>*>*]:px-4 text-center [&>*]:items-center">
-						<div className="grid grid-cols-subgrid col-span-full text-center py-2 pt-4 rounded-t font-bold border-b border-b-black mb-2 bg-prim bg-opacity-70 ">
-							<p>Hunt Name</p>
-							<p>Status</p>
-							<p>Starts In</p>
-						</div>
-						{hunts.map((hunt, ind) => (
-							<Link
-								to={{ pathname: `/${hunt.slug}/` }}
-								className="grid grid-cols-subgrid col-span-full text-center py-1 odd:bg-prim odd:bg-opacity-50 last:rounded-b hover:-translate-y-0.5 active:translate-y-0 transition-all duration-100"
-								key={hunt.id}
-							>
-								<p className="text-neutral-800 text-left p-2 font-bold">{hunt.name}</p>
-								<p>{hunt.status}</p>
-								<Countdown
-									date={new Date(hunt.start_date)}
-									renderer={countdownRenderer}
-									zeroPadDays={3}
-									className="text-2 p-2"
-								>
-									<span>The Hunt Has Commenced</span>
-								</Countdown>
-							</Link>
-						))}
-					</div>
+	return hunts?.length ? (
+		<div className="flex flex-col justify-center items-center mt-16 mx-8">
+			<div className="text-5 leading-[3rem] stroked-text-sm">Upcoming Hunts</div>
+			<div className=" rounded-md grid grid-cols-[auto_auto_auto] styled-div-1 p-0 [&>*>*]:px-4 text-center [&>*]:items-center">
+				<div className="grid grid-cols-subgrid col-span-full text-center py-3 pt-4 rounded-t font-bold border-b border-b-black mb-2 bg-prim bg-opacity-70">
+					<p>Hunt Name</p>
+					<p>Status</p>
+					<p>Starts In</p>
 				</div>
-			) : null}
+				{hunts.map((hunt, ind) => (
+					<Link
+						to={{ pathname: `/${hunt.slug}/` }}
+						className="grid grid-cols-subgrid col-span-full text-center py-2 odd:bg-prim odd:bg-opacity-50 last:rounded-b hover:-translate-y-0.5 active:translate-y-0 transition-all duration-100"
+						key={hunt.id}
+					>
+						<div className="text-left font-bold !px-8">{hunt.name}</div>
+						<p>{hunt.status}</p>
+						<Countdown
+							date={new Date(hunt.start_date)}
+							renderer={countdownRenderer}
+							zeroPadDays={3}
+							className="text-2 p-2"
+						>
+							<span>The Hunt Has Commenced</span>
+						</Countdown>
+					</Link>
+				))}
+			</div>
 		</div>
-	);
+	) : null;
 };
 
 export default OrganizingHunts;
--- src/components/RecentlyHosted.tsx
+++ src/components/RecentlyHosted.tsx
@@ -9,49 +9,88 @@ import { rootUrl } from "../utils/axios/AxiosSetup";
 import AuthContext from "../utils/context/AuthContext";
 
 const RecentlyHosted: React.FC = () => {
-  const [hunts, setHunts] = useState<Hunt[]>();
-  const [loading, setLoading] = useState<boolean>(false);
-  const contextData = useContext(AuthContext);
+	const [hunts, setHunts] = useState<(Hunt & { winner: any })[]>();
+	const [loading, setLoading] = useState<boolean>(false);
+	const contextData = useContext(AuthContext);
 
-  useEffect(() => {
-    const getRecentHunts = async () => {
-      setLoading(true);
-      try {
-        const response = await axios.get("get-recent-hunts/");
-        const data = response.data;
-        // console.log(response);
-        setHunts(data);
-        setLoading(false);
-      } catch (error) {
-        console.log(error);
-        setLoading(false);
-      }
-    };
-    getRecentHunts();
-  }, [contextData]);
-  return (
-    <div>
-      {loading && <Loading />}
-      {hunts?.length !== 0 && (
-        <div className="my-10">
-          <p className="text-3 md:text-4 m-2">Recently hosted treasure hunts</p>
-          {hunts?.map((hunt, ind) => (
-            <Link to={{ pathname: `/${hunt.slug}` }}>
-              {" "}
-              <div className="flex justify-between items-center p-2 bg-slate-200 m-2 rounded-md md:m-8">
-                <p className="flex-1 text-1 md:text-5 px-2 sm:px-10 md:px-20">
-                  {hunt.name}
-                </p>
-                <div className="flex-1 w-[300] h-[200] px-2 sm:px-10 md:px-20">
-                  <img src={`${rootUrl}${hunt.poster_img}`} alt="hunt" />
-                </div>
-              </div>
-            </Link>
-          ))}
-        </div>
-      )}
-    </div>
-  );
+	useEffect(() => {
+		const getRecentHunts = async () => {
+			setLoading(true);
+			try {
+				const response = await axios.get("get-recent-hunts/");
+				const data = response.data;
+
+				const promAll = await Promise.all(
+					data.map(async (hunt: Hunt) => {
+						try {
+							const response = await axios.get(`${hunt.slug}/leaderboard/`);
+							const data: any[] = response.data;
+
+							if (response.status === 200) {
+								return data.reduce((prevTeam, team) => {
+									if (!prevTeam) return team;
+									if (prevTeam.score < team.score) return team;
+									return prevTeam;
+								}, data[0]);
+							}
+							// console.log(response);
+						} catch (error) {
+							console.error(error);
+						}
+					})
+				);
+
+				setHunts(data.map((hunt: Hunt, ind: number) => ({ ...hunt, winner: promAll[ind] })));
+				setLoading(false);
+			} catch (error) {
+				console.log(error);
+				setLoading(false);
+			}
+		};
+		getRecentHunts();
+	}, [contextData]);
+	return (
+		<div>
+			{loading && <Loading />}
+			{hunts?.length !== 0 && (
+				<div className="my-10 w-full px-4">
+					<p className="text-5 m-2 stroked-text-sm">Recently hosted treasure hunts</p>
+					{hunts?.map((hunt, ind) => (
+						<Link
+							to={{ pathname: `/${hunt.slug}` }}
+							className="grid grid-cols-[1fr_40%] justify-between items-center styled-div-1 bg-opacity-80 my-4 gap-y-2 max-w-[60rem] mx-auto hover:-translate-y-0.5 active:translate-y-0 transition-all"
+						>
+							<div
+								className="text-4 px-2 sm:px-10 md:px-20"
+								style={{
+									gridRow: !hunt.winner ? "1 / span 2" : undefined,
+									alignSelf: hunt.winner ? "end" : undefined,
+								}}
+							>
+								{hunt.name}
+							</div>
+							<img
+								src={`${rootUrl}${hunt.poster_img}`}
+								alt="hunt"
+								className="aspect-[2/1] object-cover object-center overflow-hidden rounded row-span-2"
+							/>
+							{hunt.winner && (
+								<div className="grid grid-cols-[repeat(3,max-content)] gap-x-2 place-items-center mx-auto self-start">
+									<span className="font-bold">Winner Team</span>
+									<span>:</span>
+									<span>{hunt.winner?.team_name}</span>
+									<span className="font-bold">Leader</span>
+									<span>:</span> <span>{hunt.winner?.team_leader}</span>
+									<span className="font-bold">Points</span>
+									<span>:</span> <span>{hunt.winner?.points} </span>
+								</div>
+							)}
+						</Link>
+					))}
+				</div>
+			)}
+		</div>
+	);
 };
 
 export default RecentlyHosted;
