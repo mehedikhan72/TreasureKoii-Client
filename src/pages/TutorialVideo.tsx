48e4b2dbf6b1c2b770ac9739c17601bf34492eb8
Mustafa Muhaimin <mumufahad@gmail.com>
2024-09-11 20:10:50 +0600

    video lazy load

--- src/pages/TutorialVideo.tsx
+++ src/pages/TutorialVideo.tsx
@@ -5,7 +5,9 @@ const TutorialVideo = () => {
 		<div className="mx-auto flex flex-col justify-center items-center max-w-[60rem] px-8 mt-8">
 			<p className="text-5 stroked-text-md">Watch the tutorial video below to learn how to use TreasureKoii.</p>
 			<video
+				preload="none"
 				src={"Treasurekoii.mp4"}
+				poster="thunt-home.webp"
 				className="h-[40rem] p-0 max-sm:px-4 mt-4 m-0 sm:self-stretch styled-div-1 rounded"
 				controls
 			/>
